9581d0a11bf95ddad77ba5ca68410aeada528b2f
Wahbeeh <122154429+Wahbeeh@users.noreply.github.com>
2023-02-23 10:28:15 +0100

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,19 @@
+grade = int( input ("Hur mycket har du fått på provet ? "))
+print()
+if grade   >=90 and grade <=100:
+   print ("A+")
+elif grade >=80 and grade <=89:
+   print ("A")
+elif grade >=70 and grade <=79:
+   print ("B")
+elif grade >=60 and grade <=69:
+   print ("C")
+elif grade >=50 and grade <=59:
+   print ("D")
+elif grade >= 0 and grade <= 49:
+   print("Fel ")
+else:
+   print ("typ the correct grade ")
+
+
+
